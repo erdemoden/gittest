8f15b07e528760dbdb54c3b25b6ecde1cb93d662
erdemoden <erdemoden5@gmail.com>
2020-10-01 19:06:35 +0300

    değişti

---
+++ gittestproject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  gittestproject
+//
+//  Created by erdem öden on 1.10.2020.
+//  Copyright © 2020 erdem öden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("git")
+        print("github")
+    }
+
+
+}
+
